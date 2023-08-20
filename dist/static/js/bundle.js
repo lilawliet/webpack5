536d0f543b123909af68606f71ff4cea3961fb52
DESKTOP-ST0GILC\w2517 <25175731@qq.com>
2023-08-20 21:03:18 +0800

    解决冲突

--- dist/static/js/bundle.js
+++ dist/static/js/bundle.js
@@ -1,1 +1,31 @@
-!function(){"use strict";console.log(1-2),console.log(function(){for(var n=arguments.length,o=new Array(n),e=0;e<n;e++)o[e]=arguments[e];return o.reduce(((n,o)=>n+o),0)}(1,2,3,4))}();
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/js/count.js
+function count(x, y) {
+  return x - y;
+}
+;// CONCATENATED MODULE: ./src/js/sum.js
+function sum() {
+  for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+    args[_key] = arguments[_key];
+  }
+  return args.reduce((p, c) => p + c, 0);
+}
+;// CONCATENATED MODULE: ./src/main.js
+
+
+
+// 想要 webpack 打包资源，必须引入
+
+
+
+
+
+
+// var a = 1;  // eslint 报错
+console.log(count(1, 2));
+console.log(sum(1, 2, 3, 4));
+/******/ })()
+;
